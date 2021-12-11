ad211aed3b24df390189372321e035c37d58dd84
devrimcavusoglu <devrimcavusoglu@gmail.com>
2021-12-11 23:52:12 +0300

    WIP: 213-pa2 bst imeplementations.

--- 213/pa2/BST.h
+++ 213/pa2/BST.h
@@ -33,10 +33,15 @@ public: // DO NOT CHANGE THIS PART.
 
 private: // YOU MAY ADD YOUR OWN UTILITY MEMBER FUNCTIONS HERE.
     void print(BSTNode<T> *node, TraversalPlan tp) const;
+    bool isLeaf(BSTNode<T> *node);
+    int height();
+    int height(BSTNode<T> *node);
+    bool contains(BSTNode<T> *node, const T &data);
     void deleteNode(BSTNode<T> *node);
-    void insert(BSTNode<T> *node, const T &data);
+    void insert(BSTNode<T> *&node, const T &data);
     void remove(BSTNode<T> *node, const T &data);
-    BSTNode<T> *search(BSTNode<T> *node, const T &data);
+    BSTNode<T> *search(BSTNode<T> *node, const T &data) const;
+    BSTNode<T> *minNode(BSTNode<T> *node);
 
 private: // DO NOT CHANGE THIS PART.
     BSTNode<T> *root;
@@ -44,23 +49,59 @@ private: // DO NOT CHANGE THIS PART.
 
 #endif //BST_H
 
+// UDF
+
+template<class T>
+bool BST<T>::isLeaf(BSTNode<T> *node) {
+    return (node->left == NULL and node->right == NULL) ? true : false;
+}
+
+template<class T>
+int BST<T>::height() {
+    return height(root);
+}
+
+template<class T>
+int BST<T>::height(BSTNode<T> *node) {
+    if (node == NULL)
+        return -1;
+    if (isLeaf(node))
+        return 0;
+    int l_height = height(node->left);
+    int r_height = height(node->right);
+    if (l_height < r_height)
+        return r_height + 1;
+    return l_height + 1;
+}
+
+template<class T>
+bool BST<T>::contains(BSTNode<T> *node, const T &data) {
+    if (node == NULL)
+        return false;
+    if (node->data == data)
+        return true;
+    else 
+        return contains(node->left, data) or contains(node->right, data);
+}
+
 template<class T>
 void BST<T>::deleteNode(BSTNode<T> *node) {
     if (node == NULL)
         return;
-    this->deleteNode(node->left);
-    this->deleteNode(node->right);
+    deleteNode(node->left);
+    deleteNode(node->right);
     delete node;
     node = NULL;
 }
 
 template<class T>
 void BST<T>::insert(BSTNode<T> *&node, const T &data) {
-    if (this->contains(node, data))
+    if (contains(node, data))
         return;
-    if (node == NULL)
+    if (node == NULL) {
         node = new BSTNode<T>();
         node->data = data;
+    }
     else if (data < node->data)
         insert(node->left, data);
     else
@@ -69,19 +110,19 @@ void BST<T>::insert(BSTNode<T> *&node, const T &data) {
 
 template<class T>
 void BST<T>::remove(BSTNode<T> *node, const T &data) {
-    if (node == NULL or !this->contains(node, data))
+    if (node == NULL or !contains(node, data))
         return;
     else if (node->data == data)
-        this->deleteNode(node);
+        deleteNode(node);
     else {
-        this->remove(node->left, data);
-        this->remove(node->right, data);
+        remove(node->left, data);
+        remove(node->right, data);
     }
 }
 
 template<class T>
 BSTNode<T> *BST<T>::search(BSTNode<T> *node, const T &data) const {
-    if (node == NULL or !this->contains(node, data))
+    if (node == NULL or !contains(node, data))
         return NULL;
     if (node->data == data)
         return node;
@@ -91,10 +132,21 @@ BSTNode<T> *BST<T>::search(BSTNode<T> *node, const T &data) const {
         return search(node->right, data);
 }
 
+template<class T>
+BSTNode<T> *BST<T>::minNode(BSTNode<T> *node) {
+    BSTNode<T> *current = node;
+    while (current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
+
+// UDF END
 
 template<class T>
 BST<T>::BST() {
-    this->root = NULL;
+    root = NULL;
 }
 
 template<class T>
@@ -104,60 +156,66 @@ BST<T>::BST(const BST<T> &obj) {
 
 template<class T>
 BST<T>::~BST() {
-    deleteNode(this->root);
-    delete this->root;
+    deleteNode(root);
 }
 
 template<class T>
 BSTNode<T> *BST<T>::getRoot() const {
-    return this->root;
+    return root;
 }
 
 template<class T>
 bool BST<T>::isEmpty() const {
-    return (this->root == NULL) ? true : false;
+    return (root == NULL) ? true : false;
 }
 
 template<class T>
 bool BST<T>::contains(BSTNode<T> *node) const {
-    if (node == NULL)
-        return false;
-    if (node->data == val)
-        return true;
-    else 
-        return contains(node->left, val) or contains(node->right, val);
+    return contains(root, node->data);
 }
 
 template<class T>
 void BST<T>::insert(const T &data) {
-    this->insert(this->root, data);
+    insert(root, data);
 }
 
 template<class T>
 void BST<T>::remove(const T &data) {
-    this->remove(this->root, data);
+    remove(root, data);
 }
 
 template<class T>
 void BST<T>::removeAllNodes() {
-    this->deleteNode(this->root);
+    deleteNode(root);
 }
 
 template<class T>
 BSTNode<T> *BST<T>::search(const T &data) const {
-    this->search(this->root, data);
+    return search(root, data);
 }
 
 template<class T>
 BSTNode<T> *BST<T>::getSuccessor(BSTNode<T> *node, TraversalPlan tp) const {
-
+    BSTNode<T> *successor = root;
     if (tp == inorder) {
-        return node;
+        if (node->right != NULL)
+            return minNode(node->right);
+        while (successor != NULL) {
+            if (node->data < successor->data)
+                successor = successor->left;
+            else if (successor->data < node->data)
+                successor = successor->right;
+            else
+                break;
+        }
+        return successor;
     } else if (tp == preorder) {
         return node->left;
     } else if (tp == postorder) {
         return node->right;
     }
+    else
+        return NULL;
 }
 
 template<class T>
@@ -181,28 +239,28 @@ void BST<T>::print(TraversalPlan tp) const {
         // check if the tree is empty?
         if (isEmpty()) {
             // the tree is empty.
-            std::cout << "BST_inorder{}" << std::endl;
+            std::cout << "BST_preorder{}" << std::endl;
             return;
         }
 
         // the tree is not empty.
 
         // recursively output the tree.
-        std::cout << "BST_inorder{" << std::endl;
+        std::cout << "BST_preorder{" << std::endl;
         print(root, preorder);
         std::cout << std::endl << "}" << std::endl;
     } else if (tp == postorder) {
         // check if the tree is empty?
         if (isEmpty()) {
             // the tree is empty.
-            std::cout << "BST_inorder{}" << std::endl;
+            std::cout << "BST_postorder{}" << std::endl;
             return;
         }
 
         // the tree is not empty.
 
         // recursively output the tree.
-        std::cout << "BST_inorder{" << std::endl;
+        std::cout << "BST_postorder{" << std::endl;
         print(root, postorder);
         std::cout << std::endl << "}" << std::endl;
     }
@@ -210,8 +268,15 @@ void BST<T>::print(TraversalPlan tp) const {
 
 template<class T>
 BST<T> &BST<T>::operator=(const BST<T> &rhs) {
-    this->removeAllNodes();
-    
+    removeAllNodes();
+    if (rhs.isEmpty())
+        return *this;
+
+    BSTNode<T> *rhs_current = rhs.getSuccessor(rhs.getRoot(), inorder);
+    while (rhs_current != NULL) {
+        insert(rhs_current->data);
+    }
+    return *this;
 }
 
 template<class T>
@@ -237,8 +302,29 @@ void BST<T>::print(BSTNode<T> *node, TraversalPlan tp) const {
         }
         print(node->right, inorder);
     } else if (tp == preorder) {
-        /* TODO */
+        // first, output the node.
+        std::cout << "\t" << node->data;
+
+        // then, output left subtree and comma (if needed).
+        if (node->left) {
+            std::cout << "," << std::endl;
+        }
+        print(node->left, preorder);
+        
+
+        // finally, output comma (if needed) and the right subtree.
+        if (node->right) {
+            std::cout << "," << std::endl;
+        }
+        print(node->right, preorder);
     } else if (tp == postorder) {
-        /* TODO */
+        // then, output left subtree and comma (if needed).
+        print(node->left, postorder);
+        print(node->right, postorder);
+
+        // first, output the node.
+        std::cout << "\t" << node->data;
+        if (node != root)
+            std::cout << "," << std::endl;
     }
 }
--- 213/pa2/main_bst.cpp
+++ 213/pa2/main_bst.cpp
@@ -2,6 +2,40 @@
 
 #include "BST.h"
 
+void print_test_begin(std::string test_name) {
+    std::cout << "===== BEGIN " << test_name << " =====\n";
+}
+
+void print_test_end() {
+    std::cout << "===== END OF TEST ======\n";
+}
+
+template <class T>
+void test_case_3(BST<T> tree) {
+    print_test_begin("Test functions on empty tree..");
+    /*BSTNode<T> *s_node = tree.search(5);
+    tree.remove(5);
+    tree.insert(5);*/
+    tree.print(inorder);
+    print_test_end();
+}
+
+template <class T>
+void test_case_4(BST<T> &tree) {
+    print_test_begin("Test insertion, deletion, printing..");
+    tree.insert(4.5);
+    tree.insert(2);
+    tree.insert(6);
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(7);
+    tree.print(inorder);
+    tree.removeAllNodes();
+    tree.print(inorder);
+    print_test_end();
+}
+
 int main() {
     BST<int> tree;
 
@@ -9,17 +43,8 @@ int main() {
     tree.print(preorder);
     tree.print(postorder);
 
-    tree.insert(4);
-    tree.insert(2);
-    tree.insert(6);
-    tree.insert(1);
-    tree.insert(3);
-    tree.insert(5);
-    tree.insert(7);
-
-    tree.print(inorder);
-    tree.print(preorder);
-    tree.print(postorder);
+    test_case_3(tree);
+    test_case_4(tree);
 
     return 0;
 }
