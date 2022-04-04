7bd7d03757f06ed792e037a9fd29c033dfe1fc1a
devrimcavusoglu <devrimcavusoglu@gmail.com>
2022-04-04 03:55:35 +0300

    Parsed input bug corrected (segfault).

--- 334/hw-solutions/hw1/bundle.h
+++ 334/hw-solutions/hw1/bundle.h
@@ -7,21 +7,49 @@
 class ProcessBundle {
 public:
     std::string name;
+    std::vector<std::string> commands;
+    
     ProcessBundle(std::string bundle_name) {
         name = bundle_name;
     }
 
     // Add a command to the process bundle.
-    void addCommand(std::string cmd);
+    void addCommand(std::string &cmd) {
+        commands.push_back(cmd);
+    }
 
-    std::vector<char> getCommands();
+    int count() const {
+        return commands.size();
+    }
+
+    std::vector<std::string> getCommands() {
+        return commands;
+    }
 
     // Executes the bundle with the given commands.
-    void execute();
+    void execute() {
+    }
+};
 
-private:
-    std::vector<char> commands;
 
+class BundleControlBlock {
+public:
+    std::vector<ProcessBundle*> bundles;
+    
+    BundleControlBlock() {}
+
+    // Add a bundle to the control block.
+    void add(ProcessBundle *pb) {
+        bundles.push_back(pb);
+    }
+
+    int count() const {
+        return bundles.size();
+    }
+
+    std::vector<ProcessBundle*> all() {
+        return bundles;
+    }
 };
 
 #endif //BUNDLE_H
--- 334/hw-solutions/hw1/main.cpp
+++ 334/hw-solutions/hw1/main.cpp
@@ -1,52 +1,62 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <iomanip>
 
-#include "parser.c"
+#include "parser.h"
+#include "bundle.h"
 
 
 int main() {
 	int sts;
-	parsed_input *p;
+	parsed_input *p = new parsed_input;
 	std::string input;
 	char *init_line;
 
 	bool input_marker = true;
 	short is_bundle_creation = 0;
+	BundleControlBlock bcb;
+	ProcessBundle *pb = NULL;
 
 	while (true) {
 		if (input_marker) std::cout << ">>> ";
-		else std::cout << "(c) ";
+		else std::cout << "# ";
 
 		std::getline(std::cin >> std::ws, input);
 		
 		init_line = input.append("\n").data();
 		sts = parse(init_line, is_bundle_creation, p);
 
-		if (p->command.type == command_type::QUIT) return 0;
+		if (p->command.type == command_type::QUIT) {
+			delete p;
+			delete pb;
+			return 0;
+		}
 		else if (p->command.type == command_type::PROCESS_BUNDLE_CREATE) {
 			// PB Creation Start
 			input_marker = false;
 			is_bundle_creation = 1;
+			pb = new ProcessBundle(p->command.bundle_name);
 		}
 		else if (p->command.type == command_type::PROCESS_BUNDLE_STOP) {
 			// PB Creation End
 			input_marker = true;
 			is_bundle_creation = 0;
+			if (pb) bcb.add(pb);
 		}
 		else {
 			// Execution
-			if (is_bundle_creation) {
+			if (is_bundle_creation && pb) {
+				pb->addCommand(input);
 			}
 			else {
 			}
 		}
-		std::cout << "#################\n";
-		std::cout << "sts: " << sts << std::endl;
+		/*std::cout << "sts: " << sts << std::endl;
 		std::cout << "Type: " << p->command.type << std::endl;
 		std::cout << "Name: " << p->command.bundle_name << std::endl;
-		std::cout << "Count: " << p->command.bundle_count << std::endl;
-
-		
+		std::cout << "Count: " << p->command.bundle_count << std::endl;*/
+		std::cout << bcb.count() << ", " << pb->count() <<  std::endl;
 	}
 }
